333d3d474528400e865ea63352ea053d3fa8af1a
Damian Kądziela <damiankadziela4@gmail.com>
2024-05-26 17:31:35 +0200

    craate tasks controller

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,5 @@
+//nest g controller tasks --no-spec
+import { Controller } from '@nestjs/common';
+
+@Controller('tasks')
+export class TasksController {}
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,5 +1,8 @@
 //nest g module tasks
 import { Module } from '@nestjs/common';
+import { TasksController } from './tasks.controller';
 
-@Module({})
+@Module({
+  controllers: [TasksController],
+})
 export class TasksModule {}
