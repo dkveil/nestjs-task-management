85477d7b6ecdbed9608cd1089798e35d55b1506a
Damian Kądziela <damiankadziela4@gmail.com>
2024-05-29 19:19:19 +0200

    create tasks repository

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,12 @@
 //nest g module tasks
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { TaskRepository } from './tasks.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TaskRepository])],
   controllers: [TasksController],
   providers: [TasksService],
 })
