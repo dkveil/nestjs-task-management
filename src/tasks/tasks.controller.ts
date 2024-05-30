acf80cc466e8d3057ea6d7e7bb5766159f8e0951
Damian Kądziela <damiankadziela4@gmail.com>
2024-05-30 20:03:04 +0200

    postgres: getting task by ID

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -10,59 +10,53 @@ import { UpdateTaskDto } from './dto/update-task.dto';
 export class TasksController {
   constructor(private taskService: TasksService) {}
 
-  @Get()
-  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
-    if (Object.keys(filterDto).length) {
-      return this.taskService.getTaskWithFilters(filterDto);
-    } else {
-      return this.taskService.getAllTasks();
-    }
-  }
+  // @Get()
+  // getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+  //   if (Object.keys(filterDto).length) {
+  //     return this.taskService.getTaskWithFilters(filterDto);
+  //   } else {
+  //     return this.taskService.getAllTasks();
+  //   }
+  // }
 
   @Get('/:id')
-  getTaskById(@Param('id') id: string): Task {
-    const foundTask = this.taskService.getTaskById(id);
-
-    if (!foundTask) {
-      throw new NotFoundException(`Task with ID "${id}" not found`);
-    }
-
-    return foundTask;
+  getTaskById(@Param('id') id: string): Promise<Task> {
+    return this.taskService.getTaskById(id);
   }
 
-  @Post()
-  createTask(@Body() createTaskDto: CreateTaskDto): Task {
-    return this.taskService.createTask(createTaskDto);
-  }
+  // @Post()
+  // createTask(@Body() createTaskDto: CreateTaskDto): Task {
+  //   return this.taskService.createTask(createTaskDto);
+  // }
 
-  @Delete('/:id')
-  deleteTask(@Param('id') id: string): void {
-    this.taskService.deleteTask(id);
-  }
+  // @Delete('/:id')
+  // deleteTask(@Param('id') id: string): void {
+  //   this.taskService.deleteTask(id);
+  // }
 
-  @Patch('/:id/status')
-  updateTaskStatus(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto): Task {
-    const { status } = updateTaskDto;
+  // @Patch('/:id/status')
+  // updateTaskStatus(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto): Task {
+  //   const { status } = updateTaskDto;
 
-    if (!status) throw new NotFoundException('Status is required');
+  //   if (!status) throw new NotFoundException('Status is required');
 
-    const task = this.taskService.updateTaskStatus(id, status);
+  //   const task = this.taskService.updateTaskStatus(id, status);
 
-    if (!task) throw new NotFoundException('Task not found');
+  //   if (!task) throw new NotFoundException('Task not found');
 
-    return task;
-  }
+  //   return task;
+  // }
 
-  @Patch('/:id/description')
-  updateTaskDescription(@Param('id') id: string, @Body() UpdateTaskDto: UpdateTaskDto): Task {
-    const { description } = UpdateTaskDto;
+  // @Patch('/:id/description')
+  // updateTaskDescription(@Param('id') id: string, @Body() UpdateTaskDto: UpdateTaskDto): Task {
+  //   const { description } = UpdateTaskDto;
 
-    if (!description) throw new NotFoundException('Description is required');
+  //   if (!description) throw new NotFoundException('Description is required');
 
-    const task = this.taskService.updateTaskDescription(id, description);
+  //   const task = this.taskService.updateTaskDescription(id, description);
 
-    if (!task) throw new NotFoundException('Task not found');
+  //   if (!task) throw new NotFoundException('Task not found');
 
-    return task;
-  }
+  //   return task;
+  // }
 }
