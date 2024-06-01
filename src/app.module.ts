4118c540194557a8550ad7a37956d3c12bd2c56f
damian <dkkquality@gmail.com>
2024-06-01 14:43:00 +0200

    heroku production config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -10,15 +11,25 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true,
     }),
     TasksModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: process.env.POSTGRES_PORT,
-      username: process.env.POSTGRES_USERNAME,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB_NAME,
-      entities: [__dirname + '/**/*.entity.{js,ts}'],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => {
+        const isProduction = configService.get('STAGE') === 'production';
+
+        return {
+          ssl: isProduction,
+          extra: {
+            ssl: isProduction ? { rejectUnauthorized: false } : null,
+          },
+          type: 'postgres',
+          host: process.env.POSTGRES_HOST,
+          port: process.env.POSTGRES_PORT,
+          username: process.env.POSTGRES_USERNAME,
+          password: process.env.POSTGRES_PASSWORD,
+          database: process.env.POSTGRES_DB_NAME,
+          entities: [__dirname + '/**/*.entity.{js,ts}'],
+          synchronize: true,
+        };
+      },
     }),
     AuthModule,
   ],
